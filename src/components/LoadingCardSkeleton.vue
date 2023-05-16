35ac31701d62dc98499f1eaded10771a526c23bd
João Victor <freitasjoaovictor49@gmail.com>
2023-05-16 20:59:48 -0300

    feat(components): adding loading skeleton card component

---
+++ src/components/LoadingCardSkeleton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  containerHeight: {
+    type: String,
+    default: '',
+  },
+  bannerHeight: {
+    type: String,
+    default: 'h-30',
+  },
+})
+</script>
+
+<template>
+  <div
+    class="bg-lightGray rounded-lg shadow-lg"
+    :class="containerHeight"
+  >
+    <div
+      class="p-4 w-full bg-gray-300 animate-pulse rounded-lg"
+      :class="bannerHeight"
+    />
+  </div>
+</template>
